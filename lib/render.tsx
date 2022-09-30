484ec04f775d434974870a51d3fb68c87b53d69f
Rafi Priatna K <rafipriatnak@gmail.com>
2022-09-30 17:59:27 +0700

    Add render image notion

--- lib/render.tsx
+++ lib/render.tsx
@@ -1,11 +1,19 @@
 import Prism from "prismjs"
 import { useEffect } from "react"
+import Image from "next/image"
 import { Line } from "../components"
 import { TextInterface, BlockInterface } from "../typings/Notion"
 
 // Prism
 import "prismjs/themes/prism-okaidia.css" // Theme
-import "prismjs/components/" // Language
+// Prism Language
+import "prismjs/components/prism-javascript"
+import "prismjs/components/prism-typescript"
+import "prismjs/components/prism-python"
+import "prismjs/components/prism-bash"
+import "prismjs/components/prism-php-extras"
+
+const imagePlaceholder = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='-8 -8 40 40' fill='none' stroke='%23fff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' className='feather feather-image'%3E%3Crect x='3' y='3' width='18' height='18' rx='2' ry='2'%3E%3C/rect%3E%3Ccircle cx='8.5' cy='8.5' r='1.5'%3E%3C/circle%3E%3Cpolyline points='21 15 16 10 5 21'%3E%3C/polyline%3E%3C/svg%3E`
 
 export function Text(content: TextInterface) {
     useEffect(() => {
@@ -117,8 +125,8 @@ export function Blocks(block: BlockInterface) {
 
         case "code":
             return (
-                <pre className="not-prose max-h-96">
-                    <code className="language-javascript">
+                <pre>
+                    <code className={`language-${value.language}`}>
                         {value.rich_text[0].text.content}
                     </code>
                 </pre>
@@ -126,13 +134,57 @@ export function Blocks(block: BlockInterface) {
 
         case "callout":
             return (
-                <div className="flex space-x-4 bg-gray-50 dark:bg-slate-800 rounded-lg p-3">
+                <div className="flex space-x-4 bg-slate-800 rounded-lg p-3 my-5">
                     {value.icon && <span>{value.icon.emoji}</span>}
                     <div>
                         <Text {...value} />
                     </div>
                 </div>
             )
+
+        case "quote":
+            return (
+                <div className="max-w-4xl p-4 bg-gray-800 rounded-lg shadow bg-slate-800">
+                    <div className="mb-2">
+                        <div className="h-3 text-3xl text-left">“</div>
+                        <p className="px-4 text-center">
+                            <Text {...value} />
+                        </p>
+                        <div className="h-3 text-3xl text-right">”</div>
+                    </div>
+                </div>
+            )
+
+        case "image": {
+            const src =
+                value.type === "external" ? value.external.url : value.file.url
+            const caption =
+                value.caption.length >= 1 ? value.caption[0].plain_text : ""
+            return (
+                <figure className="mt-0">
+                    <Image
+                        className="rounded-xl"
+                        width="100%"
+                        height="100%"
+                        layout="responsive"
+                        objectFit="contain"
+                        alt={
+                            caption
+                                ? caption
+                                : "A visual depiction of what is being written about"
+                        }
+                        src={src}
+                        placeholder="blur"
+                        blurDataURL={imagePlaceholder}
+                    />
+                    {caption && (
+                        <figcaption className="text-center">
+                            {caption}
+                        </figcaption>
+                    )}
+                </figure>
+            )
+        }
 
         default:
             return `❌ Unsupported block (${
