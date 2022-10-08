bc002b1c098ad394809e9e55d3482aab274e0d51
Rafi Priatna K <rafipriatnak@gmail.com>
2022-10-08 19:07:48 +0700

    Minor update for hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,54 +18,56 @@ export default function Hero() {
     ]
 
     return (
-        <div className="grid items-center grid-cols-1 md:grid-cols-6 my-1 lg:my-20">
-            <div className="order-2 col-span-5">
-                <h1 className="text-4xl font-semibold leading-tight md:leading-normal md:order-1 max-w-lg">
-                    Hei,{" "}
-                    <span role="img" aria-label="hand">
-                        👋🏻
-                    </span>
-                    <span className="block">
-                        Saya{" "}
-                        <span className="animate-pulse text-purple">
-                            Rafi Priatna K
+        <div className="my-10 lg:my-20">
+            <div className="flex flex-row">
+                <div className="w-full lg:w-4/6">
+                    <h1 className="text-4xl font-semibold leading-tight md:leading-normal">
+                        Hei,{" "}
+                        <span role="img" aria-label="hand">
+                            👋🏻
+                        </span>
+                        <span className="block">
+                            Saya{" "}
+                            <span className="animate-pulse text-purple">
+                                Rafi Priatna K
+                            </span>
+                            .
+                        </span>
+                    </h1>
+                    <p className="text-xl leading-relaxed md:order-1 my-5 mr-5">
+                        Selamat datang di kebun digital saya{" "}
+                        <span role="img" aria-label="tanaman">
+                            🌱
                         </span>
                         .
-                    </span>
-                </h1>
-                <p className="text-xl leading-relaxed md:order-1 max-w-lg my-5">
-                    Selamat datang di kebun digital saya{" "}
-                    <span role="img" aria-label="tanaman">
-                        🌱
-                    </span>
-                    .
-                    <span className="block">
-                        Di sini akan banyak berisi tulisan random tentang saya,
-                        opini, dan kebanyakan tentang dokumentasi saya pribadi
-                        saat belajar.
-                    </span>
-                </p>
+                        <span className="block">
+                            Di sini akan banyak berisi tulisan random tentang
+                            saya, opini, dan kebanyakan tentang dokumentasi saya
+                            pribadi saat belajar.
+                        </span>
+                    </p>
 
-                {links.map((link, index) => (
-                    <a
-                        className="text-white text-lg transition duration-300 ease-in-out rounded-lg border-2 border-purple hover:bg-purple my-3 px-3 py-1 mr-3 lg:px-6 lg:py-2 lg:mr-6"
-                        href={link.href}
-                        target="_blank"
-                        rel="noreferrer"
-                        key={index}
-                    >
-                        {link.name}
-                    </a>
-                ))}
-            </div>
-
-            <div className="order-1 md:order-2 hidden md:block">
-                <Image
-                    src={Me}
-                    alt="8bit me wkwkw"
-                    className="rounded-lg"
-                    layout="responsive"
-                />
+                    {links.map((link, index) => (
+                        <a
+                            className="text-white text-lg transition duration-300 ease-in-out rounded-lg border-2 border-purple hover:bg-purple my-3 px-3 py-1 mr-3 lg:px-6 lg:py-2 lg:mr-6"
+                            href={link.href}
+                            target="_blank"
+                            rel="noreferrer"
+                            key={index}
+                        >
+                            {link.name}
+                        </a>
+                    ))}
+                </div>
+                <div className="w-2/6 relative hidden lg:block">
+                    <Image
+                        src={Me}
+                        alt="8bit me wkwkw"
+                        className="rounded-lg"
+                        objectFit="cover"
+                        layout="fill"
+                    />
+                </div>
             </div>
         </div>
     )
