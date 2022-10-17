c9a23841ca990332c801cdb0c981278400a65326
Rafi Priatna K <rafipriatnak@gmail.com>
2022-10-17 13:30:19 +0700

    Add proyek page

---
+++ pages/proyek.tsx
@@ -0,0 +1,8 @@
+export default function Proyek() {
+    return (
+        <>
+            <h1 className="text-5xl text-center font-semibold my-10">Proyek</h1>
+            <p className="text-center text-2xl my-20">🙏Segera hadir.</p>
+        </>
+    )
+}
